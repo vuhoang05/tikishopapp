4e4b4b663a81b7e95278027d9a5dd7b5068253c3
huyhoang <hoangvuzz0512@gmail.com>
2025-04-08 15:21:41 +0700

    add tailwind and fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="flex bg-red-100">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
